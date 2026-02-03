28c261ad179de86243f0954986eb03450689e859
Emil Mirzazada <55949159+Emillock@users.noreply.github.com>
2026-02-03 09:17:04 +0400

    Refactor LibraryApp to include DBManager and Connector

--- src/main/java/az/edu/ada/modules/module02/lecture04/LibraryApp.java
+++ src/main/java/az/edu/ada/modules/module02/lecture04/LibraryApp.java
@@ -1,12 +1,59 @@
 package az.edu.ada.modules.module02.lecture04;
 
-public class LibraryApp {
+class Connector{
+    public String conn=null;
+    
+    public Connector(){
+    }
+    
+    public void connect(String url){
+        conn=url;
+    }
+    
+    public void disconnect(){
+        conn=null;
+    }
+}
 
-    public void saveBook() {
-        // fetch from db: 1
+class DBManager{
+    static private Connector connector= new Connector();
+    
+    public DBManager(){
+    }
+    
+    static public int connect(String url){
+        if (DBManager.connector.conn!=null){
+            return 1;
+        }
+        DBManager.connector.connect("url");
+        return 0;
+    }
+    
+    static public int disconnect(){
+        if(DBManager.connector.conn==null){
+            return 1;
+        }
+        DBManager.connector.disconnect();
+        return 0;
+    }
+}
 
-        // do internal process
+class DB{
+    public DB(){
+        
+    }
+    
+    public void addBook(){
+        DBManager.connect("book");
+        // add book logic
+        System.out.println("added book");
+        DBManager.disconnect();
+    }
+}
 
-        // save into db: 2
+class Main {
+    public static void main(String[] args) {
+        DB db=new DB();
+        db.addBook();
     }
 }
